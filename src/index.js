9b64bd2a50b2a4a759a00021c4498d68a05ce2c9
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-03-21 03:01:32 +0530

    Signup logic with passwordencryption sone

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-// const apiRoutes = require('./routes/index');
+const apiRoutes = require('./routes/index');
 
 // const db = require('./models/index');
 
@@ -10,10 +10,10 @@ const app = express();
 
 const prepareAndStartServer = () => {
 
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-    // app.use('/api', apiRoutes);
+    app.use('/api', apiRoutes);
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
