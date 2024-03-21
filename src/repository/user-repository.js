aa8f42625b8714e839d068f506e1cafe03b581e6
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-03-21 11:35:00 +0530

    Added get user by id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -29,19 +29,20 @@ class UserRepository {
             throw error;
         }
     }
-}
-//     async getById(userId) {
-//         try {
-//             const user = await User.findByPk(userId, {
-//                 attributes: ['email', 'id']
-//             });
-//             return user;
-//         } catch (error) {
-//             console.log("Something went wrong on repository layer");
-//             throw error;
-//         }
-//     }
 
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
+}
 //     async getByEmail(userEmail) {
 //         try {
 //             const user = await User.findOne({where: {
